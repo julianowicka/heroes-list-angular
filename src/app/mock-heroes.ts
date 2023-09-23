de4443e40c06ac3e152f1195700845b9c89b58a3
julia nowicka <julia.nowicka@lemondev.pl>
2023-09-23 19:26:09 +0200

    Create mock heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Spider-Man' },
+  { id: 13, name: 'Wolverine' },
+  { id: 14, name: 'Thor' },
+  { id: 15, name: 'Cyclops' },
+  { id: 16, name: 'Iron Man' },
+  { id: 17, name: 'Daredevil' },
+  { id: 18, name: 'Hulk' },
+  { id: 19, name: 'Silver Surfer' },
+  { id: 20, name: 'Captain America' }
+];
