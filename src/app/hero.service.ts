ca424d77842c0490901df83621c30d9bab28adc8
julia nowicka <julia.nowicka@lemondev.pl>
2023-10-02 22:41:29 +0200

    Import HttpClient and HttpHeaders, inject HttpClient into the constructor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,10 +6,18 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({ providedIn: 'root' })
 export class HeroService {
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
